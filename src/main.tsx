9016a5c59f99aefcd5a0104314a058783bd4f1e5
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-11-04 15:11:55 -0800

    added redirect to vercel.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+// TODO : make custom 404 page
+// TODO: add gsap animations maybe??
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
